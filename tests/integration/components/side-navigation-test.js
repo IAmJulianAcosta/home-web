e6a86cec67ac19d5d0b66e884d8107f00919c389
nick shook <nicholas.shook@gmail.com>
2017-02-15 00:09:34 -0800

    pass tests

--- tests/integration/components/side-navigation-test.js
+++ tests/integration/components/side-navigation-test.js
@@ -10,7 +10,7 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  // this.render(hbs`{{side-navigation}}`);
+  this.render(hbs`{{side-navigation}}`);
 
-  // assert.equal(this.$().text().trim(), '');
+  assert.equal(this.$('.toolbar-title').text().trim(), 'The Apothecary Shoppe');
 });
