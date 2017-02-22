c20c27476736c045c7c345d0131f3f5c78c3bf46
nick shook <nicholas.shook@gmail.com>
2017-02-21 21:37:03 -0800

    another staff update

--- app/controllers/location-and-hours.js
+++ app/controllers/location-and-hours.js
@@ -3,6 +3,6 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   lat: 36.116026,
   lng: -115.196742,
-  zoom: 10,
+  zoom: 12,
   emberConfLocation: [36.116026, -115.196742]
 });
